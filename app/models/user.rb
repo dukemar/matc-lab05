dac226c5057480dcd6c28fadb590f516e11f8d3b
Charles Schomaker <your.cschomaker@madisoncollege.edu>
2015-03-05 05:11:05 +0000

    migrated email index

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   validates :name, presence: true, length: { minimum: 3 }, uniqueness: true
   validates :email, presence: true
+  has_secure_password
 end
 # re-do, make a new model with only name and password, not email
