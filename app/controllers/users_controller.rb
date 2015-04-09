e117ad74906060d49134fda260d2990fbc945a9c
Charles Schomaker <your.cschomaker@madisoncollege.edu>
2015-04-09 05:53:54 +0000

    put in login form and conditional header menu

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,17 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
- #   debugger
+    #debugger
+    respond_to do |format|
+      format.html
+      format.xml { render :xml => @user.to_xml }
+      format.json {
+        render :json => @user.to_json
+      }
+      format.pdf { render :pdf => @user.to_pdf }
+    end
   end
+
   def new
     @user = User.new
   end 
@@ -12,6 +21,7 @@ class UsersController < ApplicationController
       flash[:success] = 'Welcome to Chazmatic!'
       redirect_to @user
     else
+      
       render 'new'
     end
   end
